924c8879c017bd4f5a882d35401842cf4fb5d5f3
KoffeeJava <85138593+KoffeeJava@users.noreply.github.com>
2025-07-27 15:01:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
 /*
-    This is a Public Beta Version and is not made to be used for regular projects bus insted for testing.
+    This is a Nightly Version and is not made to be used for regular projects bus insted for testing.
     Please get the extension via the releases or the discord server, https://discord.gg/JpEQJkyRgX
     
-    Please read the license before modifying and distributing
-    https://tinyurl.com/tuxtility
+    MIT License
+
+    Copyright (c) 2025 KoffeeJava
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
 (function (Scratch) {
@@ -524,7 +543,7 @@
             return Scratch.fetch('https://raw.githubusercontent.com/KoffeeJava/Entity-lol/refs/heads/main/secret/Tux.txt').then(response => response.text()).then(text => text).catch(error => alert('An error has occured! \n ' + error, '\n\n Make sure that your internet is on.'))
         }
         THIS() {
-            return "Beta 3.0"
+            return "Nightly 3.0"
         }
     }
     Scratch.extensions.register(new TuxtiltiyJS());
